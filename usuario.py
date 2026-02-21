6acd003d500e37ef8b0fab77cbd289052cb73b14
RenanVieiraGd <renanggvv@gmail.com>
2026-02-20 23:15:05 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,11 +1,15 @@
 class Usuario:
-  def __init__(self, mome, email, senha):
+  def __init__(self, mome, matricula, email, senha):
     self.__nome = nome
+    self.__matricula = matricula
     self.__email = email
     senha.__senha = senha
 
   def getNome(self):
     return nome
+
+  def getMatricula(self):
+    return matricula
 
   def getEmail():
     return email
@@ -15,6 +19,9 @@ class Usuario:
 
   def setNome(self, nome):
     self.__nome = nome
+
+  def setMatricula(self, matricula)
+    self,__matricula = matricula
 
   def setEmail(self, email):
     self.__email = email
