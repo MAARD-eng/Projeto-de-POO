364b0e883e5b95c5bdc7a6d4daea7ca4e21a596f
RenanVieiraGd <renanggvv@gmail.com>
2026-02-20 22:42:05 -0300

    Add Usuario class with attributes and methods
    Implement Usuario class with private attributes and getter/setter methods for name, email, and password.

---
+++ usuario.py
@@ -0,0 +1,23 @@
+class Usuario:
+  def __init__(self):
+    self.__nome = nome
+    self.__email = email
+    senha.__senha = senha
+
+  def getNome(self):
+    return nome
+
+  def getEmail():
+    return email
+
+  def getSenha(self):
+    return senha
+
+  def setNome(self, nome):
+    self.__nome = nome
+
+  def setEmail(self, email):
+    self.__email = email
+
+  def setSenha(self, senha):
+    self.__senha = senha
